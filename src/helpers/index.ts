3704c334e41994ad99fd83373600879fab4da520
Marcos Eizayaga <marcoseizayaga@gmail.com>
2022-10-13 14:00:39 -0400

    fix notion tasks

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -3,20 +3,21 @@ import type { MDInstance } from "./types";
 export type Module<T> = () => Promise<T>;
 
 export const fetchPostImportModules = () => {
-  return Object.values(import.meta.glob("../posts/*.md") as any) as Module<MDInstance>[];
-}
+  return Object.values(
+    import.meta.glob("../posts/*.md") as any
+  ) as Module<MDInstance>[];
+};
 
 export const updatePostUrl = (post: MDInstance): MDInstance => ({
   ...post,
-  url: `/posts/${post.frontmatter.slug}`
-})
+  url: `/posts/${post.frontmatter.slug}`,
+});
 
-export const isValidPost = (post: MDInstance) => 
+export const isValidPost = (post: MDInstance) =>
   !post.frontmatter.draft && !post.frontmatter.archive;
 
-export const postFilter = (post: MDInstance) : MDInstance | [] => 
-  (!post.frontmatter.draft && !post.frontmatter.archive) ? post : [];
-
+export const postFilter = (post: MDInstance): MDInstance | [] =>
+  !post.frontmatter.draft && !post.frontmatter.archive ? post : [];
 
 export const postComparator = (a: MDInstance, b: MDInstance) =>
   new Date(b.frontmatter.date).getTime() -
@@ -24,67 +25,95 @@ export const postComparator = (a: MDInstance, b: MDInstance) =>
 
 export const sortPosts = (posts: MDInstance[]) => posts.sort(postComparator);
 
-export const filterAndSortPosts = (posts: MDInstance[]) : MDInstance[] => 
-  sortPosts(posts.filter(isValidPost))
+export const filterAndSortPosts = (posts: MDInstance[]): MDInstance[] =>
+  sortPosts(posts.filter(isValidPost));
 
 export type TaskGroupId = string;
-export interface TaskGroupInfo { id: TaskGroupId; name: string; color: string; }
-export type Task = { groupId: TaskGroupId, name: string; lastEditedTime: Date; }
-export type TaskGroup = { groupInfo: TaskGroupInfo, tasks: Task[], numberOfUnloadedPosts: number };
+export interface TaskGroupInfo {
+  id: TaskGroupId;
+  name: string;
+  color: string;
+}
+export type Task = { groupId: TaskGroupId; name: string; lastEditedTime: Date };
+export type TaskGroup = {
+  groupInfo: TaskGroupInfo;
+  tasks: Task[];
+  numberOfUnloadedPosts: number;
+};
 export type TaskBoard = Map<string, TaskGroup>;
 export type QueryDatabaseResponse = { results: QueryTaskDBEntry[] };
 export interface QueryTaskDBEntry {
-  last_edited_time: string,
+  last_edited_time: string;
   properties: {
-    Status: { select: TaskGroupInfo; };
-    Name: { title: { text: { content: string } }[]; };
+    Status: { select: TaskGroupInfo };
+    Name: { title: { text: { content: string } }[] };
   };
 }
-export interface GetDatabaseResponse { 
-  properties: { Status: { select: { options: TaskGroupInfo[]; } } }
+export interface GetDatabaseResponse {
+  properties: { Status: { select: { options: TaskGroupInfo[] } } };
 }
 
-export const DefaultTaskGroup = (groupInfo: TaskGroupInfo) : TaskGroup => ({
-  groupInfo, tasks: [], numberOfUnloadedPosts: 0
+export const DefaultTaskGroup = (groupInfo: TaskGroupInfo): TaskGroup => ({
+  groupInfo,
+  tasks: [],
+  numberOfUnloadedPosts: 0,
 });
-export const DefaultTaskBoard = (numOfGroups: number) : TaskBoard => 
-  new Map<TaskGroupId, TaskGroup>(new Array(numOfGroups).fill(undefined).map((v, i) => [i.toString(), v]));
-export const DefaultTaskBoardWithGroupInfos = (groupInfos: TaskGroupInfo[]) : TaskBoard => 
-  new Map<TaskGroupId, TaskGroup>(groupInfos.map(info => [info.id, DefaultTaskGroup(info)]))
+export const DefaultTaskBoard = (numOfGroups: number): TaskBoard =>
+  new Map<TaskGroupId, TaskGroup>(
+    new Array(numOfGroups).fill(undefined).map((v, i) => [i.toString(), v])
+  );
+export const DefaultTaskBoardWithGroupInfos = (
+  groupInfos: TaskGroupInfo[]
+): TaskBoard =>
+  new Map<TaskGroupId, TaskGroup>(
+    groupInfos.map((info) => [info.id, DefaultTaskGroup(info)])
+  );
 export const sortTasks = (a: Task, b: Task) =>
   b.lastEditedTime.getTime() - a.lastEditedTime.getTime();
 export const MAX_NUMBER_OF_TASKS_PER_GROUP = 4;
-export const FillTaskBoard = (board: TaskBoard, tasks: Task[], allPosts: boolean) => {
+export const FillTaskBoard = (
+  board: TaskBoard,
+  tasks: Task[],
+  allPosts: boolean
+) => {
   tasks.forEach((task) => {
     const prevGroup = board.get(task.groupId);
-    prevGroup && board.set(task.groupId, {
-      ...prevGroup,
-      tasks: prevGroup.tasks.concat(task)
-    });
+    prevGroup &&
+      board.set(task.groupId, {
+        ...prevGroup,
+        tasks: prevGroup.tasks.concat(task),
+      });
   });
   board.forEach((v, k, m) => {
     let tasks = v.tasks.sort(sortTasks);
     let numberOfUnloadedPosts = 0;
     if (!allPosts) {
       tasks = tasks.slice(0, MAX_NUMBER_OF_TASKS_PER_GROUP);
-      numberOfUnloadedPosts = Math.max(v.tasks.length - MAX_NUMBER_OF_TASKS_PER_GROUP, 0);
+      numberOfUnloadedPosts = Math.max(
+        v.tasks.length - MAX_NUMBER_OF_TASKS_PER_GROUP,
+        0
+      );
     }
-    m.set(k, {...v, tasks, numberOfUnloadedPosts})
-  })
-}
-export const TaskBoardToResponseBody = (board: TaskBoard): string => 
+    m.set(k, { ...v, tasks, numberOfUnloadedPosts });
+  });
+};
+export const TaskBoardToResponseBody = (board: TaskBoard): string =>
   JSON.stringify({ board: JSON.stringify(Array.from(board)) });
-export const ResponseJsonToTaskBoard = (json: any): TaskBoard => 
+export const ResponseJsonToTaskBoard = (json: any): TaskBoard =>
   new Map<TaskGroupId, TaskGroup>(JSON.parse(json.board));
 
-export const parseTasks = (db: QueryDatabaseResponse) : Task[] => {
-  const taskEntries: QueryTaskDBEntry[] = db.results.slice(0, -1);
-  return taskEntries.map(v => ({
+const validQueryTaskDBEntry = (e: QueryTaskDBEntry) =>
+  !!e.properties.Status.select;
+
+export const parseTasks = (db: QueryDatabaseResponse): Task[] => {
+  const taskEntries: QueryTaskDBEntry[] = db.results.filter(
+    validQueryTaskDBEntry
+  );
+  return taskEntries.map((v) => ({
     groupId: v.properties.Status.select.id,
     name: v.properties.Name.title[0].text.content,
-    lastEditedTime: new Date(v.last_edited_time)
+    lastEditedTime: new Date(v.last_edited_time),
   }));
 };
-export const parseGroups = (db: GetDatabaseResponse) : TaskGroupInfo[] => (
-  db.properties.Status.select.options
-)
+export const parseGroups = (db: GetDatabaseResponse): TaskGroupInfo[] =>
+  db.properties.Status.select.options;
--- src/pages/notion-tasks.ts
+++ src/pages/notion-tasks.ts
@@ -7,33 +7,43 @@ import {
   parseGroups,
   DefaultTaskBoardWithGroupInfos,
   FillTaskBoard,
-  TaskBoardToResponseBody
+  TaskBoardToResponseBody,
 } from "../helpers";
 
 export async function post({ request }: any) {
   const data = await request.json();
 
   if (data.database_id === undefined) {
-    return new Response(JSON.stringify({ 
-      statusText: '"database_id" not defined in request body'
-    }), { status: 400});
+    return new Response(
+      JSON.stringify({
+        statusText: '"database_id" not defined in request body',
+      }),
+      { status: 400 }
+    );
   }
-  
-  const notion = new Client({ auth: import.meta.env.NOTION_SECRET })
+
+  const notion = new Client({ auth: import.meta.env.NOTION_SECRET });
   let tasks: Task[];
   let groupInfos: TaskGroupInfo[];
-  
+
   try {
-    tasks = await notion.databases.query({ 
-      database_id: data.database_id
-    }).then((r: any) => parseTasks(r));
-    groupInfos = await notion.databases.retrieve({ 
-      database_id: data.database_id
-    }).then((r: any) => parseGroups(r));
+    tasks = await notion.databases
+      .query({
+        database_id: data.database_id,
+      })
+      .then((r: any) => parseTasks(r));
+    groupInfos = await notion.databases
+      .retrieve({
+        database_id: data.database_id,
+      })
+      .then((r: any) => parseGroups(r));
   } catch (e) {
-    return new Response(JSON.stringify({ 
-      statusText: 'Database not found'
-    }), { status: 400});
+    return new Response(
+      JSON.stringify({
+        statusText: "Database not found",
+      }),
+      { status: 400 }
+    );
   }
 
   const taskBoard: TaskBoard = DefaultTaskBoardWithGroupInfos(groupInfos);
